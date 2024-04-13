01069586bf47ee46de2550a6fe1599aa437d1ddd
Smaug123 <patrick+github@patrickstevens.co.uk>
2024-04-13 10:44:14 +0100

    Workflow

---
+++ README.md
@@ -0,0 +1,3 @@
+# Yoneda Lemma
+
+This is a [Manim](https://docs.manim.community/) explanation of the Yoneda lemma.
--- flake.nix
+++ flake.nix
@@ -6,49 +6,171 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachSystem [ "aarch64-darwin" ] (system:
-      let
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
         pkgs = import nixpkgs {
           inherit system;
         };
-      in
-      let manim-tinytex = pkgs.texliveInfraOnly.withPackages (ps: with ps; [
+      in let
+        manim-tinytex = pkgs.texliveInfraOnly.withPackages (ps:
+          with ps; [
+            # tinytex
+            amsfonts
+            amsmath
+            atbegshi
+            atveryend
+            auxhook
+            babel
+            bibtex
+            bigintcalc
+            bitset
+            booktabs
+            cm
+            dehyph
+            dvipdfmx
+            dvips
+            ec
+            epstopdf-pkg
+            etex
+            etexcmds
+            etoolbox
+            euenc
+            everyshi
+            fancyvrb
+            filehook
+            firstaid
+            float
+            fontspec
+            framed
+            geometry
+            gettitlestring
+            glyphlist
+            graphics
+            graphics-cfg
+            graphics-def
+            grffile
+            helvetic
+            hycolor
+            hyperref
+            hyph-utf8
+            iftex
+            inconsolata
+            infwarerr
+            intcalc
+            knuth-lib
+            kvdefinekeys
+            kvoptions
+            kvsetkeys
+            l3backend
+            l3kernel
+            l3packages
+            latex
+            latex-amsmath-dev
+            latex-bin
+            latex-fonts
+            latex-tools-dev
+            latexconfig
+            latexmk
+            letltxmacro
+            lm
+            lm-math
+            ltxcmds
+            lua-alt-getopt
+            luahbtex
+            lualatex-math
+            lualibs
+            luaotfload
+            luatex
+            mdwtools
+            metafont
+            mfware
+            natbib
+            pdfescape
+            pdftex
+            pdftexcmds
+            plain
+            psnfss
+            refcount
+            rerunfilecheck
+            stringenc
+            tex
+            tex-ini-files
+            times
+            tipa
+            tools
+            unicode-data
+            unicode-math
+            uniquecounter
+            url
+            xcolor
+            xetex
+            xetexconfig
+            xkeyval
+            xunicode
+            zapfding
 
-    # tinytex
-    amsfonts amsmath atbegshi atveryend auxhook babel bibtex
-    bigintcalc bitset booktabs cm dehyph dvipdfmx dvips ec epstopdf-pkg etex
-    etexcmds etoolbox euenc everyshi fancyvrb filehook firstaid float fontspec
-    framed geometry gettitlestring glyphlist graphics graphics-cfg graphics-def
-    grffile helvetic hycolor hyperref hyph-utf8 iftex inconsolata infwarerr
-    intcalc knuth-lib kvdefinekeys kvoptions kvsetkeys l3backend l3kernel
-    l3packages latex latex-amsmath-dev latex-bin latex-fonts latex-tools-dev
-    latexconfig latexmk letltxmacro lm lm-math ltxcmds lua-alt-getopt luahbtex
-    lualatex-math lualibs luaotfload luatex mdwtools metafont mfware natbib
-    pdfescape pdftex pdftexcmds plain psnfss refcount rerunfilecheck stringenc
-    tex tex-ini-files times tipa tools unicode-data unicode-math uniquecounter
-    url xcolor xetex xetexconfig xkeyval xunicode zapfding
-
-    # manim-latex
-    standalone everysel preview doublestroke ms setspace rsfs relsize ragged2e
-    fundus-calligra microtype wasysym physics dvisvgm jknapltx wasy cm-super
-    babel-english gnu-freefont mathastext cbfonts-fd
-  ]);
-in
-      {
+            # manim-latex
+            standalone
+            everysel
+            preview
+            doublestroke
+            ms
+            setspace
+            rsfs
+            relsize
+            ragged2e
+            fundus-calligra
+            microtype
+            wasysym
+            physics
+            dvisvgm
+            jknapltx
+            wasy
+            cm-super
+            babel-english
+            gnu-freefont
+            mathastext
+            cbfonts-fd
+          ]);
+      in let
+        pythonEnv = pkgs.python3.withPackages (ps: [
+          ps.numpy
+          ps.pycairo
+          ps.pip
+          ps.mapbox-earcut
+          ps.scipy
+          ps.srt
+          ps.pygments
+          ps.requests
+        ]);
+      in {
         devShell = pkgs.mkShell {
-          buildInputs = [
-            pkgs.darwin.apple_sdk.frameworks.OpenGL
-            pkgs.darwin.apple_sdk.frameworks.ApplicationServices
-            pkgs.darwin.apple_sdk.frameworks.Carbon
-            pkgs.cairo
-            pkgs.pango
-            pkgs.pkg-config
-            pkgs.glib
-            pkgs.python3
-            pkgs.ffmpeg
-            manim-tinytex
-          ];
+          buildInputs =
+            (
+              if pkgs.stdenv.isDarwin
+              then [
+                pkgs.darwin.apple_sdk.frameworks.OpenGL
+                pkgs.darwin.apple_sdk.frameworks.ApplicationServices
+                pkgs.darwin.apple_sdk.frameworks.Carbon
+              ]
+              else []
+            )
+            ++ [
+              pythonEnv
+              pkgs.cairo
+              pkgs.pango
+              pkgs.pkg-config
+              pkgs.glib
+              pkgs.ffmpeg
+              pkgs.nodePackages.markdown-link-check
+              pkgs.alejandra
+              manim-tinytex
+            ];
         };
       }
     );
